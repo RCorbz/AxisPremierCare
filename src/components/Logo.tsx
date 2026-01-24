df92ba291803bfc2239788758c4c56a1f964135c
Rich Corbz <rich.corbz@gmail.com>
2026-01-24 12:27:40 -0700

    chore: Wipe codebase for V2 Rebuild

--- src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-export default function Logo({ className = "w-8 h-8" }: { className?: string }) {
-    return (
-        <img
-            src="/logo.png"
-            alt="Axis Premier Care Logo"
-            className={`${className} object-contain`}
-        />
-    );
-}
